de24a757a374e37e85eace915d639066c267ba87
Marvan <marvansm@code.edu.az>
2025-11-05 19:44:06 +0400

    _

--- client/src/Featured/Section/CompaniesSection/index.tsx
+++ client/src/Featured/Section/CompaniesSection/index.tsx
@@ -29,20 +29,30 @@ const CompaniesSection = () => {
           <CompaniesCard />
           <CompaniesCard />
         </div>
-        <div className="grid grid-cols-2">
-          <div
-            className="border border-[#514fc0] p-12 bg-cover bg-no-repeat"
-            style={{
-              backgroundImage: `
-      url('https://cdn.prod.website-files.com/6499e85.../649b8a2..._dot-bg-card-feature-jobboardly-x-webflow-template.png'),
-      linear-gradient(to top, #5e5bff 16%, var(--accent--primary-1))
-    `,
-              backgroundPosition: "0 0, 0 0",
-              backgroundRepeat: "no-repeat, repeat",
-              backgroundSize: "cover, auto",
-            }}
-          >
-            asdasdalsdaldkaldklasd
+        <div className="grid grid-cols-2 gap-8 pt-[100px] pb-2.5">
+          <div className="border border-[#514fc0] p-12 bg-[#514fc0] rounded-md ">
+            <h2 className="mb-1 text-[22px] text-white font-semibold leading-[31.988px]">
+              Post a featured job
+            </h2>
+            <p className="opacity-[.8] text-white mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit tincidunt
+              sit venenatis, vulputate tristique fringilla ut.
+            </p>
+            <button className="rounded-sm px-3 py-2 text-[14px] leading-[17.99px] bg-white border border-[#eaebf3] shadow-sm font-medium hover:scale-[1.01] duration-200 cursor-pointer">
+              See all companies
+            </button>
+          </div>
+          <div className="border border-[#F2F2F7] p-12 bg-[#F2F2F7] rounded-md">
+            <h2 className="mb-1 text-[22px] text-black font-semibold leading-[31.988px]">
+              Post a free job
+            </h2>
+            <p className="opacity-[.8] text-[#484b62] mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit tincidunt
+              sit venenatis, vulputate tristique fringilla ut.
+            </p>
+            <button className="rounded-sm px-3 py-2 text-[14px] leading-[17.99px] bg-white border border-[#eaebf3] shadow-sm font-medium hover:scale-[1.01] duration-200 cursor-pointer">
+              Post a free job
+            </button>
           </div>
         </div>
       </div>
