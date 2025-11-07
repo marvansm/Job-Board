0a50d72bb97841831420f21fae15fdf7839aef8b
Marvan <marvansm@code.edu.az>
2025-11-07 19:50:27 +0400

    _

--- client/src/Router/routes.tsx
+++ client/src/Router/routes.tsx
@@ -1,7 +1,6 @@
 import { createRootRoute, createRoute } from "@tanstack/react-router";
 import Layout from "../Layout";
 import Home from "../Featured/Pages/Home";
-import Detail from "../Featured/Pages/detail";
 
 export const rootRoute = createRootRoute({
   component: Layout,
@@ -12,12 +11,7 @@ export const HomeRouter = createRoute({
   path: "/",
   component: Home,
 });
-export const VacancyDetailRouter = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/vacancy/$id",
-  component: Detail,
-});
+
 export const routeTree = rootRoute.addChildren([
   HomeRouter,
-  VacancyDetailRouter,
 ]);
